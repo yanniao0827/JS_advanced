86b0260bf372d49c080723e7cab002eb344a1f3c
yanniao0827 <yanning0113niao@gmail.com>
2024-05-21 15:25:04 +0800

    module的export

--- modules/person2.js
+++ modules/person2.js
@@ -1,3 +1,5 @@
+console.log(import.meta.url);
+
 export default class Person2{
     gender="female"; //可以直接設定屬性
     constructor(name="Lea",age=20){
@@ -11,3 +13,8 @@ export default class Person2{
 }
 
 console.log("person2 module要export");
+
+// export : 一般的匯出
+export const PI = 3.14;
+const PI2 = 3.14159;
+export const b = (n) => n * n * n;
